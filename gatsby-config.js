d18189a723de697fde99b01e7ab26e363075d178
vladi <vladi@aresgate.net>
2020-10-14 12:56:06 -0700

    making blog default

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `RedEyeCPA Blog`,
+    author: `Daria Nagal`,
+    description: `Blog`,
+    siteUrl: `https://blog.redeyecpa.com/`,
     social: {
       twitter: `gatsbyjs`,
     },
